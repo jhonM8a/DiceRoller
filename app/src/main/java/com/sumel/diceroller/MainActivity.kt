93f5734f27e90cf2ffd196df5ca8732bc64a805d
jhonochoa <jhon.ochoa@unillanos.edu.co>
2020-03-29 18:24:40 -0500

    Se añade id para el TextView, y con el Onclick del button se cambia a un número aleatorio

--- app/src/main/java/com/sumel/diceroller/MainActivity.kt
+++ app/src/main/java/com/sumel/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.sumel.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,8 +14,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton : Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{
-            
-            Toast.makeText(this, "button Clicked", Toast.LENGTH_SHORT).show()
+            /*Esto muestra un popup corto en la actividad indicada
+            Toast.makeText(this, "button Clicked", Toast.LENGTH_SHORT).show()}
+            */
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultText : TextView = findViewById(R.id.restult_text)
+        val ramdomInt = Random().nextInt(6)+1
+        resultText.text =  ramdomInt.toString()
     }
 }
