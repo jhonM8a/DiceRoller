e49a2b01a947be06ed8c27d16ad0f7613c2edd21
jhonochoa <jhon.ochoa@unillanos.edu.co>
2020-03-29 18:10:59 -0500

    Se añade un Id al button y se la da un OnClick encargado de mostrar un popup de corta duración

--- app/src/main/java/com/sumel/diceroller/MainActivity.kt
+++ app/src/main/java/com/sumel/diceroller/MainActivity.kt
@@ -2,11 +2,18 @@ package com.sumel.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton : Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{
+            
+            Toast.makeText(this, "button Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
